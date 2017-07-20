b32d44de999677b31281fcd83f9cbdbbac3b79c3
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-07-20 18:03:54 +0200

    Start parsing GraphemeBreakProperty-file into groups

---
+++ unicode-data/read_property_file.py
@@ -0,0 +1,44 @@
+import json
+import os
+import re
+from collections import defaultdict
+
+
+BREAK_PROPERTY_FILE = "GraphemeBreakProperty.txt"
+BREAK_PROPERTY_JSON_FILE = "../grapheme/data/grapheme_break_property.json"
+
+pattern = re.compile("^([0-9A-F]+)(\.\.([0-9A-F]+))?\s+;\s*(\w*)\s*#")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+class Group:
+    single_chars = []
+    ranges = []
+def group():
+    return dict(
+        single_chars=[],
+        ranges=[]
+    )
+
+chardata = defaultdict(group)
+
+with open(os.path.join(dir_path, BREAK_PROPERTY_FILE), "r") as file:
+    for line in file:
+        if line.startswith("#"):
+            continue
+
+        match = pattern.search(line)
+        if not match:
+            continue
+
+        (start, _, end, group_name) = match.groups()
+
+        if end is None:
+            chardata[group_name]["single_chars"].append(start)
+        else:
+            chardata[group_name]["ranges"].append((start, end))
+
+        # print((start, end, group_name))
+
+with open(os.path.join(dir_path, BREAK_PROPERTY_JSON_FILE), "w") as out_file:
+    out_file.write(json.dumps(chardata, indent=2))
