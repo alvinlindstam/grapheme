94c96e8697790fbd33a9bbe7b927afc362f39822
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-07-28 00:23:16 +0200

    Increase string length in benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,11 +6,11 @@ import timeit
 def random_ascii_string(n):
     return "".join(choice(ascii_lowercase) for i in range(n))
 
-long_ascii_string = random_ascii_string(1000)
+long_ascii_string = random_ascii_string(10000)
 
 statements = [
     ("len(long_ascii_string)", 1000),
-    ("grapheme.length(long_ascii_string)", 1000),
+    ("grapheme.length(long_ascii_string)", 500),
 ]
 for statement, n in statements:
     result = timeit.timeit(statement, setup="from __main__ import long_ascii_string; import grapheme", number=n) / 100
