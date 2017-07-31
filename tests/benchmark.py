d27cf9fdfdf853b5933499ac065c9533ad75d521
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-07-31 22:12:39 +0200

    Add more benchmarks

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -11,6 +11,14 @@ long_ascii_string = random_ascii_string(10000)
 statements = [
     ("len(long_ascii_string)", 1000),
     ("grapheme.length(long_ascii_string)", 500),
+    ("grapheme.length(long_ascii_string, 500)", 1000),
+    ("long_ascii_string[0:100]", 1000),
+    ("grapheme.slice(long_ascii_string, 0, 100)", 1000),
+    ("long_ascii_string[:100] in long_ascii_string", 1000),
+    ("grapheme.contains(long_ascii_string, long_ascii_string[:100])", 500),
+    ("long_ascii_string[-100:] in long_ascii_string", 1000),
+    ("grapheme.contains(long_ascii_string, long_ascii_string[-100:])", 500),
+
 ]
 for statement, n in statements:
     result = timeit.timeit(statement, setup="from __main__ import long_ascii_string; import grapheme", number=n) / 100
