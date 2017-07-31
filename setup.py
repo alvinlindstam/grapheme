be6e9d6f7376491a7247b8933bd51c90eb2c0e4e
Alvin Lindstam <alvin.lindstam@gmail.com>
2017-07-31 22:35:31 +0200

    Bump patch version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='grapheme',
-      version='0.2.0',
+      version='0.2.1',
       description=u"Unicode grapheme helpers",
       long_description=long_description,
       keywords='',
